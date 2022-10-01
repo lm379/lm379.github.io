41638438531e7574abe0b1161d3c2d45f424223b
lm379 <lm379@qq.com>
2022-10-01 21:54:52 +0800

    Site updated: 2022-10-01 21:54:51

--- dist/music.js
+++ dist/music.js
@@ -13,16 +13,30 @@ const ap = new APlayer({
         {
             "title": "起风了",
             "author": "买辣椒也用券",
-            "url": "https://pan.lm379.cn/d/music/%E4%B9%B0%E8%BE%A3%E6%A4%92%E4%B9%9F%E7%94%A8%E5%88%B8%20-%20%E8%B5%B7%E9%A3%8E%E4%BA%86.mp3",
+            "url": "https://pan.lm379.cn/d/ali/Music/%E4%B9%B0%E8%BE%A3%E6%A4%92%E4%B9%9F%E7%94%A8%E5%88%B8%20-%20%E8%B5%B7%E9%A3%8E%E4%BA%86.mp3",
             "pic": "https://y.qq.com/music/photo_new/T002R300x300M000003j3NMw1ZBpsv_2.jpg?max_age=2592000",
-            "lrc": "https://pan.lm379.cn/d/music/%E4%B9%B0%E8%BE%A3%E6%A4%92%E4%B9%9F%E7%94%A8%E5%88%B8%20-%20%E8%B5%B7%E9%A3%8E%E4%BA%86.lrc"
+            "lrc": "https://pan.lm379.cn/d/ali/Music/lrc/%E4%B9%B0%E8%BE%A3%E6%A4%92%E4%B9%9F%E7%94%A8%E5%88%B8%20-%20%E8%B5%B7%E9%A3%8E%E4%BA%86.lrc"
         },
         {
             "title": "溯 (Reverse)",
             "author": "CORSAK胡梦周、马吟吟",
-            "url": "https://pan.lm379.cn/d/music/CORSAK%E8%83%A1%E6%A2%A6%E5%91%A8%E3%80%81%E9%A9%AC%E5%90%9F%E5%90%9F%20-%20%E6%BA%AF%20(Reverse).flac",
-            "lrc": "https://pan.lm379.cn/d/music/CORSAK%E8%83%A1%E6%A2%A6%E5%91%A8%E3%80%81%E9%A9%AC%E5%90%9F%E5%90%9F%20-%20%E6%BA%AF%20(Reverse).lrc",
+            "url": "https://pan.lm379.cn/d/ali/Music/CORSAK%E8%83%A1%E6%A2%A6%E5%91%A8%E3%80%81%E9%A9%AC%E5%90%9F%E5%90%9F%20-%20%E6%BA%AF%20(Reverse).flac",
+            "lrc": "https://pan.lm379.cn/d/ali/Music/lrc/CORSAK%E8%83%A1%E6%A2%A6%E5%91%A8%E3%80%81%E9%A9%AC%E5%90%9F%E5%90%9F%20-%20%E6%BA%AF%20(Reverse).lrc",
             "pic": "https://y.qq.com/music/photo_new/T002R300x300M000002yOiJ347ly2o_1.jpg?max_age=2592000"
+        },
+        {
+            "title": "打上花火",
+            "author": "DAOKO、米津玄師",
+            "url": "https://pan.lm379.cn/d/ali/Music/DAOKO%E3%80%81%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%20-%20%E6%89%93%E4%B8%8A%E8%8A%B1%E7%81%AB.mp3",
+            "pic": "https://imgessl.kugou.com/stdmusic/20170810/20170810111013866169.jpg",
+            "lrc": "https://pan.lm379.cn/d/ali/Music/lrc/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%E3%80%81DAOKO%20-%20%E6%89%93%E4%B8%8A%E8%8A%B1%E7%81%AB.lrc"
+        },
+        {
+            "title": "错位时空",
+            "author": "艾辰",
+            "url": "https://pan.lm379.cn/d/ali/Music/%E8%89%BE%E8%BE%B0%20-%20%E9%94%99%E4%BD%8D%E6%97%B6%E7%A9%BA.flac",
+            "pic": "https://bkimg.cdn.bcebos.com/pic/a8ec8a13632762d0f7037ac288a71ffa513d26976830?x-bce-process=image/resize,m_lfit,w_536,limit_1/format,f_jpg",
+            "lrc": "https://pan.lm379.cn/d/ali/Music/lrc/%E8%89%BE%E8%BE%B0%20-%20%E9%94%99%E4%BD%8D%E6%97%B6%E7%A9%BA.lrc"
         }
 	]
 });
